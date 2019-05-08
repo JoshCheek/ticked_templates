685ad740f6fc55e33d5c0364c91ff0ad907c38dc
Josh Cheek <josh.cheek@gmail.com>
2019-05-08 17:46:18 -0500

    More refactoring

--- lib/ticked/templates.rb
+++ lib/ticked/templates.rb
@@ -4,47 +4,53 @@ require 'ripper'
 
 module Ticked
   module Templates
-    extend self # for Templates.`
-
-    refine Object do
-      def `(template_str, binding: binding().of_caller(1))
-        Templates.`(template_str, binding: binding) #`
-      end
-    end
+    extend self
 
     def `(template_str, binding: binding().of_caller(1))
       strings, expressions = Templates.parse(template_str)
       interpolations = expressions.map &binding.method(:eval)
       Template.new strings: strings, interpolations: interpolations
     end
-  end
 
-  class << Templates
-    def parse(template_str)
+    refine Object do
+      def `(template_str, binding: binding().of_caller(1))
+        Templates.`(template_str, binding: binding) #` comment is here to fix editor highlighting
+      end
+    end
+
+    OPEN  = ["$", "{"].map(&:freeze).freeze
+    CLOSE = ["}"].map(&:freeze).freeze
+    private_constant :OPEN
+    private_constant :CLOSE
+
+    def self.parse(template_str)
       stream, strings, exprs = StringIO.new(template_str), [], []
       loop do
-        strings << scan_until(stream, "${") { true }
+        strings << scan_until(stream, OPEN) { true }
         break if stream.eof?
-        exprs << scan_until(stream, "}") { |expr| valid? expr }
+        exprs << scan_until(stream, CLOSE) { |expr| valid? expr }
       end
       [strings, exprs]
     end
 
-    private
-
-    def scan_until(stream, delimiter)
-      str, buffer = "", []
-      delimiter.size.times { stream.eof? || buffer << stream.getc }
+    private_class_method def self.scan_until(stream, delim)
+      scanned, buffer = "", []
+      delim.size.times { stream.eof? || buffer << stream.getc }
       loop do
-        buffstr = buffer.join("")
-        return str if buffstr == delimiter && yield(str)
-        return str << buffstr if stream.eof? # uhhm, could probably mask a syntax error, don't think that's tested
-        str    << buffer.shift
-        buffer << stream.getc
+        return scanned if match?(delim, buffer) && yield(scanned)
+        return scanned << buffer.join("") if stream.eof? # uhhm, could probably mask a syntax error, don't think that's tested
+        scanned << buffer.shift
+        buffer  << stream.getc
       end
     end
 
-    def valid?(code)
+    private_class_method def self.match?(buffer, delimiter)
+      buffer.each_index.all? do |index|
+        buffer[index] == delimiter[index]
+      end
+    end
+
+    private_class_method def self.valid?(code)
       Ripper.sexp code
     end
   end

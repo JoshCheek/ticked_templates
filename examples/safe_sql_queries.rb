d4fed140b513bc18984ee3418fda3454e6665448
Josh Cheek <josh.cheek@gmail.com>
2019-05-08 15:25:52 -0500

    Safe SQL example supports nested queries

--- examples/safe_sql_queries.rb
+++ examples/safe_sql_queries.rb
@@ -12,17 +12,20 @@ User = Class.new(ActiveRecord::Base) { has_many :posts  }
 Post = Class.new(ActiveRecord::Base) { belongs_to :user }
 
 # Seed Data
-User.create! name: 'Kanye', posts: [Post.new(name: 'Keep ya love lockdown')]
-User.create! name: 'Keanu', posts: [Post.new(name: 'Life is good when you’ve a good sandwich')]
-user = User.create! name: 'Josh', posts: [Post.new(name: 'yo ho ho'), Post.new(name: 'and a bottle of rum')]
+kanye = User.create! name: 'Kanye', posts: [Post.new(name: 'Keep ya love lockdown')]
+keanu = User.create! name: 'Keanu', posts: [Post.new(name: 'Life is good when you’ve a good sandwich')]
+josh  = User.create! name: 'Josh',  posts: [Post.new(name: 'yo ho ho'), Post.new(name: 'and a bottle of rum')]
 
 # Potential lib code that wires ticked templates into Sqlite
 class TemplatedSqlite
   def initialize(connection)
     @connection = connection
   end
-  def exec(strings:, interpolations:)
-    @connection.execute(strings.join('?'), interpolations).map do |row|
+  def exec(template)
+    template = template.flatten
+    query    = template.strings.join(??)
+    binds    = template.interpolations
+    @connection.execute(query, binds).map do |row|
       row.each_with_object({}) do |(key, value), attributes|
         attributes[key.intern] = value if key.is_a? String
       end
@@ -38,9 +41,18 @@ using Ticked::Templates
 
 # We can now write SQL, but the interpolations are kept separate, so this is safe
 template = <<~`SQL`
-  SELECT id, name FROM posts WHERE user_id = ${user.id}
+  SELECT id, name FROM posts WHERE user_id = ${josh.id}
 SQL
 template                # => `SELECT id, name FROM posts WHERE user_id = ${3}\n`
 template.strings        # => ["SELECT id, name FROM posts WHERE user_id = ", "\n"]
 template.interpolations # => [3]
 sqlite.exec template    # => [{:id=>3, :name=>"yo ho ho"}, {:id=>4, :name=>"and a bottle of rum"}]
+
+# We can even compose templates
+query1 = `select name from posts where user_id = ${kanye.id}`
+query2 = `select name from posts where user_id = ${josh.id}`
+sqlite.exec <<~`SQL`  # => [{:name=>"Keep ya love lockdown"}, {:name=>"and a bottle of rum"}, {:name=>"yo ho ho"}]
+  ${query1}
+  UNION
+  ${query2}
+SQL

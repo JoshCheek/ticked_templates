8da436f8ff8505b316873fea690d52dd07692a41
Josh Cheek <josh.cheek@gmail.com>
2019-05-08 14:22:40 -0500

    Move spec helper code into common file

--- spec/ticked_templates_spec.rb
+++ spec/ticked_templates_spec.rb
@@ -1,22 +1,4 @@
-require 'ticked'
-
-module SpecHelpers
-  def eq!(expected, actual)
-    actual = actual.to_str  if expected.is_a?(String) && actual.respond_to?(:to_str)
-    actual = actual.to_hash if expected.is_a?(Hash)   && actual.respond_to?(:to_hash)
-    expect(actual).to eq expected
-  rescue RSpec::Expectations::ExpectationNotMetError
-    $!.set_backtrace caller.drop(1)
-    raise
-  end
-end
-
-RSpec.configure do |config|
-  config.include SpecHelpers
-  config.color     = true
-  config.formatter = 'documentation'
-  config.fail_fast = true
-end
+require 'spec_helper'
 
 RSpec.describe Ticked::Templates do
   def `(str)
